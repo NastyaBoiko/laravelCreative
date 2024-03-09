b4b090e9e261b12fb4d9ddefd94c55ff7dab4da8
Nastya <killer99whale@mail.ru>
2024-03-09 22:25:49 +0300

    first view

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,11 +8,10 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index() {
-        $posts = Post::where('likes', '>', 15)->get();
-        foreach ($posts as $post) {
-            dump($post->likes);
-        }
-        dd($posts);
+        $posts = Post::all();
+        return view('posts', compact('posts'));
+        // либо так
+        // return view('posts', ['posts' => $posts]);
     }
 
     public function create() {
