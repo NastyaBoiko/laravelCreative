e396431f3a52c44c33a4f7e93fc77fd8d4e563da
Nastya <killer99whale@mail.ru>
2024-03-11 13:33:04 +0300

    bootstrap

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,18 +5,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
-    <div>
-        <nav>
-            <ul>
-                <li><a href="{{ route('main.index') }}">Main</a></li>
-                <li><a href="{{ route('post.index') }}">Posts</a></li>
-                <li><a href="{{ route('about.index') }}">About</a></li>
-                <li><a href="{{ route('contact.index') }}">Contacts</a></li>
-            </ul>
-        </nav>
+    <div class="container">
+        <div class="row">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ route('main.index') }}">Main</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="{{ route('post.index') }}">Posts</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="{{ route('about.index') }}">About</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="{{ route('contact.index') }}">Contacts</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+            </nav>
+        </div>
+        @yield('content')
     </div>
-    @yield('content')
 </body>
 </html>
